b0027358122bf8382ed806151c4ea00db448face
18kiku <18kiku@gmail.com>
2022-09-15 18:33:59 +0900

    Initial commit

---
+++ src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,36 @@
+package org.zerock.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+
+@Log4j
+public class JDBCTests {
+
+    static {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection() {
+
+        try(Connection con =
+                    DriverManager.getConnection(
+                            "jdbc:oracle:thin:@localhost:1521:XE",
+                            "system",
+                            "0707"
+                    )){
+            System.out.println(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
